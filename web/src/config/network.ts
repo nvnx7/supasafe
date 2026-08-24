be5c0bdd7953fbbe083ce76c26df6543e864e99a
nvnx7 <thenvnsahu@gmail.com>
2026-08-24 20:38:40 +0530

    chore: update network config

--- web/src/config/network.ts
+++ web/src/config/network.ts
@@ -1,10 +1,6 @@
 import { type Chain, devnet } from "@starknet-start/chains";
 import { constants } from "starknet";
-import {
-  network,
-  rpcUrlDevnet,
-  rpcUrlSepolia,
-} from "./env";
+import { network, rpcUrlDevnet, rpcUrlSepolia } from "./env";
 
 export type NetworkType = "devnet" | "sepolia";
 
@@ -13,7 +9,7 @@ export const DEVNET_CHAIN_ID = "0x4445564e4554"; // "DEVNET" in hex
 export type NetworkConfig = {
   rpcUrl: string;
   chainId: string;
-  udcAddress: string,
+  udcAddress: string;
   privacyPoolAddress: string;
   multisigClassHash: string;
 };
@@ -30,17 +26,21 @@ export const devnetChain: Chain = {
 const devnetConfig: NetworkConfig = {
   rpcUrl: rpcUrlDevnet,
   chainId: DEVNET_CHAIN_ID,
-  udcAddress: '0x2CEED65A4BD731034C01113685C831B01C15D7D432F71AFB1CF1634B53A2125',
-  privacyPoolAddress: '',
-  multisigClassHash: '0x710c8af32df683660c7ca3eb7ddb6c20a226d59687842bca8c0bb3ec838d2fa'
+  udcAddress:
+    "0x2CEED65A4BD731034C01113685C831B01C15D7D432F71AFB1CF1634B53A2125",
+  privacyPoolAddress: "",
+  multisigClassHash:
+    "0x5859c2d7eba82496d85ee68c409a156f5467c8ca753ca47acf9a98c42734878",
 };
 
 const sepoliaConfig: NetworkConfig = {
   rpcUrl: rpcUrlSepolia,
   chainId: constants.StarknetChainId.SN_SEPOLIA,
-  udcAddress: '',
-  privacyPoolAddress: '',
-  multisigClassHash: ''
+  udcAddress: "",
+  privacyPoolAddress:
+    "0x0254a6b2997ef52e9f830ce1f543f6b29768295e8d17e2267d672c552cfe0d91",
+  multisigClassHash:
+    "0x5859c2d7eba82496d85ee68c409a156f5467c8ca753ca47acf9a98c42734878",
 };
 
 export const networkConfigs: Record<NetworkType, NetworkConfig> = {
