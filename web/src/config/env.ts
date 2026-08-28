df580120200b1eb072cc9b9b03a3ebbcbe716430
nvnx7 <thenvnsahu@gmail.com>
2026-08-28 17:24:39 +0530

    chore: add provider and indexer urls

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -1,10 +1,15 @@
 export type Network = "devnet" | "sepolia";
 
-export const network: Network =
-  (process.env.NEXT_PUBLIC_STARKNET_NETWORK as Network) || "devnet";
+export const network = "sepolia" as Network;
+
+// export const network: Network =
+//   (process.env.NEXT_PUBLIC_STARKNET_NETWORK as Network) || "devnet";
 
 export const isDevnet = network === "devnet";
 export const isSepolia = network === "sepolia";
 
 export const rpcUrlDevnet = process.env.NEXT_PUBLIC_RPC_URL_DEVNET as string;
 export const rpcUrlSepolia = process.env.NEXT_PUBLIC_RPC_URL_SEPOLIA as string;
+
+export const proverUrl = process.env.NEXT_PUBLIC_PROVER_URL as string;
+export const indexerUrl = process.env.NEXT_PUBLIC_INDEXER_URL as string;
