3ac7bf3c949d5ffab24a47108759e8c97b8ba702
nvnx7 <thenvnsahu@gmail.com>
2026-08-31 21:12:36 +0530

    chore: add indexer mainnet url

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -17,5 +17,7 @@ export const proverUrlSepolia = process.env
   .NEXT_PUBLIC_PROVER_URL_SEPOLIA as string;
 export const indexerUrlSepolia = process.env
   .NEXT_PUBLIC_INDEXER_URL_SEPOLIA as string;
+export const indexerUrlMainnet = process.env
+  .NEXT_PUBLIC_INDEXER_URL_MAINNET as string;
 export const apiKeyStarkscan = process.env
   .NEXT_PUBLIC_API_KEY_STARKSCAN as string;
