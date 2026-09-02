4ca5d0885ac23390e152b95ad3c6ea7077f723b4
nvnx7 <thenvnsahu@gmail.com>
2026-09-02 21:09:09 +0530

    chore: fill config for vesu

--- web/src/config/dapp.ts
+++ web/src/config/dapp.ts
@@ -17,7 +17,7 @@ export type EkuboConfig = {
 };
 
 export type VesuVaultConfig = {
-  underlyingToken: string;
+  underlyingTokenAddress: string;
   vTokenAddress: string;
 };
 
@@ -75,7 +75,22 @@ export const vesuSepoliaConfig: VesuConfig = {
 
 export const vesuMainnetConfig: VesuConfig = {
   anonymizerAddress: "",
-  vaults: [],
+  vaults: [
+    // STRK
+    {
+      underlyingTokenAddress:
+        "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
+      vTokenAddress:
+        "0x4cd290592b3520ced3951d55e2c7f036832f6f9579b4dfe3a80f91ccda2ae8a",
+    },
+    // ETH
+    {
+      underlyingTokenAddress:
+        "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+      vTokenAddress:
+        "0x6811b89c6cac2726b62e872c4ff776c4d6538a544dc9a9b6b7a14350f4170e4",
+    },
+  ],
 };
 
 const ekuboConfigs: Record<Network, EkuboConfig> = {
